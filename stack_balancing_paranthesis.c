efa2b7a8922662ef579bc99ce7b6c2f4e1e27faa
Dharshan <dharshanms09@gmail.com>
2025-10-23 13:20:07 +0530

    2exp

---
+++ stack_balancing_paranthesis.c
@@ -0,0 +1,75 @@
+//
+// Created by Dharshan on 23-10-2025.
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 100
+
+char stk[MAX];
+int top=-1;
+
+void push(char ele)
+{
+    if (top==MAX)
+    {
+        printf("Overflow stack\n");
+        return;
+    }
+    stk[++top]=ele;
+}
+
+char pop()
+{
+    if (top==-1)
+    {
+        printf("Stack empty\n");
+        return '\0';
+    }
+    return stk[top--];
+}
+
+int matching_pair(char char1,char char2)
+{
+    return (char1=='[' && char2==']')||(char1=='(' && char2==')') || (char1=='{' && char2=='}');
+}
+
+int balance(char *exp)
+{
+    for (int i=0;i<strlen(exp);i++)
+    {
+        if (exp[i]=='(' || exp[i]=='[' || exp[i]=='{')
+        {
+            push(exp[i]);
+        }
+        else if (exp[i]==')' || exp[i]==']' || exp[i]=='}')
+        {
+            if (top==-1)
+            {
+                return 0;
+            }
+            if (!matching_pair(pop(),exp[i]))
+            {
+                return 0;
+            }
+        }
+    }
+    return top==-1;
+}
+
+void main()
+{
+    char ipexp[MAX];
+    printf("\nEnter the expression: ");
+    gets(ipexp);
+    if (balance(ipexp))
+    {
+        printf("Expression is balanced");
+    }
+    else
+    {
+        printf("Expression is not balanced");
+    }
+}
